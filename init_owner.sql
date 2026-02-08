9f943219a7ac21cbe41e48569ea52daca8f63e75
lizhi123le <34964850+lizhi123le@users.noreply.github.com>
2026-02-08 19:10:30 +0800

    Delete init_owner.sql

--- init_owner.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO users (username, password_hash, role, banned, tags, oidc_sub, enabled_apis, created_at, playrecord_migrated, favorite_migrated, skip_migrated)
-VALUES ('lizhi', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'owner', 0, NULL, NULL, NULL, 1770491489932, 1, 1, 1)
-ON CONFLICT(username) DO UPDATE SET password_hash = excluded.password_hash, role = 'owner';
