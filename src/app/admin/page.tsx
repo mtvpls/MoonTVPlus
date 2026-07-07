3559339c59167298a6bfffbaca0fda3470bd103f
kaicheng <guizhouwudao@gmail.com>
2026-07-07 08:40:34 +0800

    fix: Pansou toggle className syntax (escaped quotes -> double quotes)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5276,7 +5276,7 @@ const NetDiskConfigComponent = ({
                 onChange={(e) => setPansouEnabled(e.target.checked)}
                 className='sr-only peer'
               />
-              <div className=\"w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600\"></div>
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
             </label>
           </div>
 
