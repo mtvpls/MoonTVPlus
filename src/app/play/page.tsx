847c525c7859d200ae6cded74f02778da7425156
MurasameCyan <50967145+MurasameCyan@users.noreply.github.com>
2026-08-17 00:47:05 +0800

    feat(play): 更新快进倒退时间设置图标

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -7557,7 +7557,7 @@ function PlayPageClient() {
             {
               name: '快进/倒退时间',
               html: '快进/倒退时间',
-              icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 5v14" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/><path d="m16 17 5-5-5-5" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M21 12H9" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/></svg>',
+              icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m4 12 6-6v12l-6-6Z" fill="#ffffff"/><path d="m20 12-6-6v12l6-6Z" fill="#ffffff"/></svg>',
               tooltip: `${formatQuickForwardDuration(seekStepSecondsRef.current)}`,
               onClick: async function () {
                 const player = artPlayerRef.current;
